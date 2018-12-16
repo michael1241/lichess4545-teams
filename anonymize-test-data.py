dc528fbd130f6b361eec62f64c2d43cb05c6b0d2
Martin Colwell <colwem@gmail.com>
2018-12-15 20:20:23 -0500

    change string.replace to re.sub to avoid potential substring matching bug

--- anonymize-test-data.py
+++ anonymize-test-data.py
@@ -1,7 +1,7 @@
 import click
 import random
 import json
-
+import re
 
 
 @click.command()
@@ -12,20 +12,21 @@ def run(players):
         players = json.load(infile)
 
     names = [p['name'] for p in players]
-
     for i, name in enumerate(names):
         for player in players:
             replaceName = 'p{}'.format(i)
             if player['name'] == name:
                 player['name'] = replaceName
-            player['friends'] = player['friends'].replace(name, replaceName)
+            player['friends'] = re.sub(r"\b{}\b".format(name),
+                                       replaceName,
+                                       player['friends'])
 
     ratings = [player['rating'] for player in players]
     random.shuffle(ratings)
     for player, rating in zip(players, ratings):
         player['rating'] = rating
 
-    print(json.dumps(players))
+    print(json.dumps(players, indent=4))
 
 
 if __name__ == "__main__":
